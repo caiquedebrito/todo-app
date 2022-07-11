bc372bc968bf5c783cf56afda678f6bd61b75874
Caique De Brito <caiquedebritoo@gmail.com>
2022-07-11 19:35:14 -0300

    refactor: remove unneeded todo state logic
    - remove functions (check, remove) that handle the todo

--- src/components/AllTodos.js
+++ src/components/AllTodos.js
@@ -1,90 +1,27 @@
 import React, { useContext, useEffect } from "react";
 import { TodoContext } from "../App";
 import "../styles/todo-list.scss";
-import iconCross from "../images/icon-cross.svg";
+import { SingleTodo } from "./SingleTodo";
 
 export default function AllTodos() {
   const {
     todos,
-    setTodos,
-    completed,
-    setCompleted,
-    todoActive,
-    setTodoActive,
     setQuantity
   } = useContext(TodoContext);
 
   useEffect(() => {
     setQuantity(todos.length)
-  }, [todos, todoActive, completed])
+  }, [todos])
 
-  function checkTask(id, name) {
-    const n = id.toString(); // converte em string
-    const checkTask = "check-task";
 
-    const task = document.querySelector(`.todo-${n}-${name[0]}`); // pega a task com uma classe única
-    task.classList.toggle(checkTask); // quando clicado adiciona e remove o estado de completado na task
-
-    if (task.className.includes(checkTask)) {
-      // Adiciona o todo completado
-      setCompleted((prev) => [...prev, name]);
-
-      // Tira o status ativo do todo
-      const index = todoActive.indexOf(name);
-      todoActive.splice(index, 1);
-      setTodoActive([...todoActive]);
-    } else {
-      // Remove o todo completado
-      const index = completed.indexOf(name);
-      completed.splice(index, 1);
-      setCompleted(() => [...completed]);
-
-      // Adiciona o todo como ativo
-      setTodoActive((prev) => [...prev, name]);
-    }
-  }
-
-  function removeTask(key, name) {
-    todos.splice(key, 1);
-    setTodos(() => [...todos]);
-
-    const index = todoActive.indexOf(name)
-    todoActive.splice(index, 1)
-    setTodoActive(() => [...todoActive])
-
-    if (completed.includes(name)) {
-      const index = completed.indexOf(name)
-      completed.splice(index, 1)
-      setCompleted(() => [...completed])
-    }
-  }
 
   return (
-    <div className="todo-list">
-      {todos.map((todo, key) => {
-        if (completed.includes(todo)) {
-          return (
-            <div
-              className={"check-task todo todo-" + key + "-" + todo[0]}
-              key={key}
-            >
-              <div className="check-btn" onClick={() => checkTask(key, todo)}>
-                <div className="img-container"></div>
-              </div>
-              {todo}
-              <img src={iconCross} onClick={() => removeTask(key, todo)} />
-            </div>
-          );
-        } else {
-          return <div className={"todo todo-" + key + "-" + todo[0]} key={key}>
-            <div className="check-btn" onClick={() => checkTask(key, todo)}>
-              <div className="img-container"></div>
-            </div>
-            {todo}
-            <img src={iconCross} onClick={() => removeTask(key, todo)} />
-          </div>;
-        }
-      })}
-    </div>
+    <>
+      {
+        todos.map(todo => (
+          <SingleTodo todo={todo} key={todo.id}/>
+        ))
+      }
+    </>
   );
 }
--- src/components/CompletedTodo.js
+++ src/components/CompletedTodo.js
@@ -1,64 +1,28 @@
 import React, { useContext, useEffect } from 'react'
 import { TodoContext } from '../App'
 import '../styles/todo-list.scss'
-import iconCross from '../images/icon-cross.svg'
+import { SingleTodo } from './SingleTodo'
 
 export default function CompletedTodo() {
-  const {todos, setTodos, completed, setCompleted, todoActive, setTodoActive, quantity, setQuantity} = useContext(TodoContext)
+  const {todos, setQuantity} = useContext(TodoContext)
 
   useEffect(() => {
-    setQuantity(completed.length || 0)
-  }, [todos, todoActive, completed])
-
-  function checkTask(id, name) {
-    const n = id.toString() // converte em string
-    const checkTask = 'check-task'
-
-    const task = document.querySelector(`.todo-${n}-${name[0]}`) // pega a task com uma classe única
-    task.classList.toggle(checkTask) // quando clicado adiciona e remove o estado de completado na task
-
-    if (task.className.includes(checkTask)) {
-      // Adiciona o todo completado
-      setCompleted(prev => [...prev, name])
-
-      // Tira o status ativo do todo
-      const index = todoActive.indexOf(name)
-      todoActive.splice(index, 1)
-      setTodoActive([...todoActive])
-    } else {
-      // Remove o todo completado
-      const index = completed.indexOf(name)
-      completed.splice(index, 1)
-      setCompleted(()=> [...completed])
-
-      // Adiciona o todo como ativo
-      setTodoActive(prev => [...prev, name])
-    }
-
-  }
-
-  function removeTask(key, name){
+    let quantity = 0
+    todos.forEach(todo => {
+      if (todo.isCompleted) {
+        quantity++
+      }
+    })
     
-    completed.splice(key, 1)
-    setCompleted(() => [...completed])
-
-    const index = todos.indexOf(name)
-    todos.splice(index, 1)
-    setTodos(() => [...todos])
-  }
+    setQuantity(quantity)
+  }, [todos])
+  
 
   return (
-    <div className='todo-list'>
+    <>
       {
-        completed.map((todo, key) => 
-        <div className={"check-task todo todo-" + key + '-' + todo[0]} key={key}>
-          <div className='check-btn'onClick={() => checkTask(key, todo)} >
-            <div className='img-container'></div>
-          </div>
-          {todo}
-          <img src={iconCross} onClick={()=> removeTask(key, todo)}/>
-        </div>)
+        todos.map(todo => todo.isCompleted ? <SingleTodo todo={todo} key={todo.id}/> : "")
       }
-    </div>
+    </>
   )
 }
