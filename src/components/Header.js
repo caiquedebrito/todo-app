af8d5e14ec20fb20c5b972fd142fdc0b13bdf935
Caique De Brito <caiquedebritoo@gmail.com>
2022-07-14 16:20:54 -0300

    feat: add theme mode storage
    - save user-selected theme mode
    - by default the theme mode loaded is the user's computer theme

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/header.scss'
 
 export default function Header() {
-  function toggleMode() {
-    document.body.classList.toggle('light-mode')
+  const [themeMode, setThemeMode] = useState("")
+
+  useEffect(() => {
+    if (isThemeModeSet()) {
+      setThemeMode(localStorage.getItem("theme"))
+      return
+    }
+    
+    if (isLightModeSetAsDefault()) {
+      setThemeMode("light")
+      return
+    } 
+  }, [])
+
+  useEffect(changeThemeMode, [themeMode])
+
+  function isThemeModeSet() {
+    return JSON.stringify(localStorage.getItem("theme")) !== "null"
+  }
+
+  function isLightModeSetAsDefault() {
+    return window.matchMedia("(prefers-color-scheme: light)").matches
+  }
+
+  function changeThemeMode() {
     const themeBtn = document.querySelector('.theme-btn')
-    themeBtn.classList.toggle('light-icon-theme')
+
+    if (themeMode === "light") {
+      localStorage.setItem("theme", "light")
+      document.body.classList.add('light-mode')
+      themeBtn.classList.add('light-icon-theme')
+    } else if (themeMode === "dark") {
+      localStorage.setItem("theme", "dark")
+      document.body.classList.remove('light-mode')
+      themeBtn.classList.remove('light-icon-theme')
+    }
+  }
+
+  function toggleThemeMode() {
+    switch (themeMode) {
+      case "dark":
+        setThemeMode("light")
+        break
+      case "light":
+        setThemeMode("dark")
+        break
+    }
   }
 
   return (
     <header>
         <h1>Todo</h1>
-        <div className='theme-btn' onClick={toggleMode}>
+        <div className='theme-btn' onClick={toggleThemeMode}>
         </div>
     </header>
   )
